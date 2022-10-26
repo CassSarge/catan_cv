92e9a1a869c027bc9dca880a1ac5cc4c1d5947b7
Josh Wilkinson <joshlwilkinson9@gmail.com>
2022-10-26 22:42:16 +1100

    Initial training code (85% accuracy)

---
+++ src/trainCNN.py
@@ -0,0 +1,141 @@
+from venv import create
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras import layers
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+import PIL
+import numpy as np
+import os
+import sys
+import argparse
+import time
+import matplotlib.pyplot as plt
+import cv2
+
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import roc_curve, auc
+# from sklearn.metrics import roc_auc_score
+# from sklearn.metrics import precision_recall_curve
+# from sklearn.metrics import average_precision_score
+# from sklearn.metrics import precision_score
+# from sklearn.metrics import recall_score
+# from sklearn.metrics import f1_score
+
+# Path: src/trainCNN.py
+def create_dataset(data_dir, img_height=80, img_width=80, batch_size=32):
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+    )
+
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+    )
+
+    return (train_ds, val_ds)
+
+
+def main():
+    # train a network to classify images of numbers
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default="numInputs",
+        help="Directory for loading input data",
+    )
+
+    args = parser.parse_args()
+
+    img_width = 80
+    img_height = 80
+
+    train_ds, val_ds = create_dataset(args.data_dir)
+    print(train_ds.class_names)
+    print(val_ds.class_names)
+
+    num_classes = len(train_ds.class_names)
+
+    for image_batch, labels_batch in train_ds:
+        print(image_batch.shape)
+        print(labels_batch.shape)
+        print(image_batch)
+        break
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    data_augmentation = keras.Sequential(
+        [layers.RandomRotation(0.8, input_shape=(img_height, img_width, 3), fill_value = 0.0)]
+    )
+
+    model = Sequential(
+        [
+            data_augmentation,
+            layers.Rescaling(1.0 / 255),
+            layers.Conv2D(16, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Dropout(0.2),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(num_classes, name="outputs"),
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    model.summary()
+
+    epochs = 150
+    history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
+
+    acc = history.history["accuracy"]
+    val_acc = history.history["val_accuracy"]
+
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label="Training Accuracy")
+    plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+    plt.legend(loc="lower right")
+    plt.title("Training and Validation Accuracy")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label="Training Loss")
+    plt.plot(epochs_range, val_loss, label="Validation Loss")
+    plt.legend(loc="upper right")
+    plt.title("Training and Validation Loss")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
